259d03bae01e8ea1f34f99fe5409f4e69abf1dd2
Spud <10385195+yeSpud@users.noreply.github.com>
2025-02-03 22:10:21 -0900

    Bug fixes

--- initalizaton.py
+++ initalizaton.py
@@ -3,6 +3,8 @@ import re
 from bs4 import BeautifulSoup
 import json
 
+from peewee import IntegrityError
+
 from database import Route, Stop, Arrival, db
 
 
@@ -23,23 +25,40 @@ def get_data(url: str):
 def add_to_database(stops, route):
     for stop in stops:
         print(f"Adding stop {stop["address"]}")
-        Stop.create(id=stop["id"], name=stop["address"], route=route)
+        try:
+            Stop.create(id=stop["id"], name=stop["address"], route=route)
+        except IntegrityError as e:
+            print("that stop already exists in the database")
 
 db.connect()
 db.create_tables([Route, Stop, Arrival])
 
 
-'''
-print("Loading nenana data")
-nenana = get_data("https://buswhere.com/uaf/routes/nenana_morning")
-add_to_dict(nenana["stops"])
+try:
+    print("Initializing nenana data")
+    nenana_route = Route.create(name="Nenana")
+    nenana_data = get_data("https://buswhere.com/uaf/routes/nenana_morning")
+    add_to_database(nenana_data["stops"], nenana_route)
+except IntegrityError as e:
+    print("The nenana route already exists")
 
-# TODO Yukon data
-'''
+try:
+    print("Initializing yukon data")
+    yukon_route = Route.create(name="Yukon")
+    yukon_1_data = get_data("https://buswhere.com/uaf/routes/yukon_route_bus_1")
+    yukon_2_data = get_data("https://buswhere.com/uaf/routes/yukon_route_bus_2")
+    add_to_database(yukon_1_data["stops"], yukon_route)
+    add_to_database(yukon_2_data["stops"], yukon_route)
+except IntegrityError as e:
+    print("The yukon 1 route already exists")
 
-print("Initializing night data")
-night_route = Route.create(name="Night")
-night_data = get_data("https://buswhere.com/uaf/routes/night_route")
-add_to_database(night_data["stops"], night_route)
+
+try:
+    print("Initializing night data")
+    night_route = Route.create(name="Night")
+    night_data = get_data("https://buswhere.com/uaf/routes/night_route")
+    add_to_database(night_data["stops"], night_route)
+except IntegrityError as e:
+    print("The night route already exists")
 
 db.close()
--- main.py
+++ main.py
@@ -2,48 +2,49 @@ import requests
 from datetime import datetime
 import time
 
-stop_dict = {}
+from database import Stop, Arrival, db
+
+# stop_list = []
 morning_route_start = datetime(hour=7, minute=30, second=0, microsecond=0, year=1, month=1, day=1).time()
 night_route_start = datetime(hour=18, minute=0, second=0, microsecond=0, year=1, month=1, day=1).time()
-night_route_end = datetime(hour=21, minute=0, second=0, microsecond=0, year=1, month=1, day=1).time()
-
-class Stop:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-        self.arrived = False
-
-    def __str__(self):
-        return f"{self.name} ({self.id}): {self.arrived}"
+night_route_end = datetime(hour=22, minute=00, second=0, microsecond=0, year=1, month=1, day=1).time()
 
 def get_night_data():
     data = requests.get("https://buswhere.com/uaf/routes/night_route",
                         headers={"Accept": "application/json"})
     json = data.json()
     stop_etas: dict = json["stop_eta"]
-    for stop_eta in stop_etas.keys():
-        print(f"Checking {stop_dict[stop_eta].name}")
-        if stop_etas[stop_eta] == "arrived":
-            print(f"Arrived at stop {stop_dict[stop_eta].name}")
+    for stop_id in stop_etas.keys():
+        stop = Stop.get_by_id(stop_id)
+        # print(f"Checking {stop.name}") # todo better logger
+        if stop_etas[stop_id] == "arrived":
+            print(f"Arrived at stop {stop.name} at {datetime.now().time()}")
 
-            # Only rest to true if it wasnt already marked as such
-            if not stop_dict[stop_eta].arrived:
-                stop_dict[stop_eta].arrived = True
-        else:
-            stop_dict[stop_eta].arrived = False
+            # Only rest to true if it wasn't already marked as such
+            #if not stop_dict[stop_id].arrived:
+            Arrival.create(stop=stop, time=datetime.now().timestamp())
+            #    stop_dict[stop_id].arrived = True
+        #else:
+            #stop_dict[stop_id].arrived = False
 
 # Start by getting the initialization data
 # FIXME Buswhere gets rid of morning routes after 6!
 
-print(stop_dict.keys())
-print(f"Time: {datetime.now().time()}")
+try:
+    db.connect()
+    while True:
+        current_time = datetime.now().time()
+        if night_route_start <= current_time < night_route_end:
+            get_night_data()
 
-while (True):
-    current_time = datetime.now().time()
-    if night_route_start<= current_time < night_route_end:
-        get_night_data()
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("Exiting...")
+except Exception as e:
+    print(e)
 
-    time.sleep(5)
+db.close()
+
 
 # TODO Loop though each update and log if its arrived or not
 
